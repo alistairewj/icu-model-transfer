2fd27d4d045e3042696a26b531b694fb8f178a30
Alistair Johnson <alistairewj@gmail.com>
2018-04-20 09:56:45 -0400

    filter out rows with windowtime < disch/censor/death time

--- datasets/eicu/time-day1.sql
+++ datasets/eicu/time-day1.sql
@@ -3,8 +3,9 @@ CREATE TABLE tr_time_day1 AS
 select
     co.patientunitstayid
   , sd.censortime_hours, sd.deathtime_hours, sd.dischtime_hours
-  , 24::SMALLINT as windowtime
+  , 24::SMALLINT as windowtime_hours
 from tr_cohort co
 LEFT JOIN tr_static_data sd
   ON co.patientunitstayid = sd.patientunitstayid
-where co.excluded = 0;
+where co.excluded = 0
+AND 24 <= LEAST(sd.censortime_hours, sd.deathtime_hours, sd.dischtime_hours);
--- datasets/mimic/time-day1.sql
+++ datasets/mimic/time-day1.sql
@@ -4,8 +4,9 @@ CREATE TABLE tr_time_day1 AS
 select
     co.icustay_id
   , sd.censortime_hours, sd.deathtime_hours, sd.dischtime_hours
-  , 24::SMALLINT as windowtime
+  , 24::SMALLINT as windowtime_hours
 from tr_cohort co
 LEFT JOIN tr_static_data sd
   ON co.icustay_id = sd.icustay_id
-where co.excluded = 0;
+where co.excluded = 0
+AND 24 <= LEAST(sd.censortime_hours, sd.deathtime_hours, sd.dischtime_hours);
